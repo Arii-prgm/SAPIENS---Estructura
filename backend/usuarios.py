e49467bb467320e8025f56c902a0f0ae6aeecc40
Mile141203 <milenanicole2479@gmail.com>
2026-07-07 20:09:15 -0500

    usuarios en json

---
+++ backend/usuarios.py
@@ -0,0 +1,52 @@
+{
+    "administrador@live.uleam.edu.ec": {
+        "password": "admin123",
+        "role": "admin",
+        "nombre": "Administrador Sapiens"
+    },
+    "coordinador@live.uleam.edu.ec": {
+        "password": "coordinador123",
+        "role": "coordinador",
+        "nombre": "Coordinador Sapiens"
+    },
+    "docente@live.uleam.edu.ec": {
+        "password": "docente123",
+        "role": "docente",
+        "nombre": "Docente Sapiens"
+    },
+    "estudiante@live.uleam.edu.ec": {
+        "password": "estudiante123",
+        "role": "estudiante",
+        "nombre": "Estudiante Sapiens"
+    },
+    "admin@gmail.com": {
+        "password": "123456",
+        "role": "admin",
+        "nombre": "Adm. Joseph Coello"
+    },
+    "1245789635@gmail.com": {
+        "password": "123456",
+        "role": "docente",
+        "nombre": "Mariano el Ano"
+    },
+    "12457854125@gmail.com": {
+        "password": "123456",
+        "role": "estudiante",
+        "nombre": "Culiano el Mariano"
+    },
+    "7894561235@gmail.com": {
+        "password": "123456",
+        "role": "estudiante",
+        "nombre": "Alejandrito el huesito"
+    },
+    "1234564565@gmail.com": {
+        "password": "123456",
+        "role": "docente",
+        "nombre": "Yadira Montanero"
+    },
+    "1245789634@gmail.com": {
+        "password": "123456",
+        "role": "estudiante",
+        "nombre": "Sebastian Pollito"
+    }
+}
